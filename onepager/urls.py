2e3d5093ec198788a7d2942b11897a4178bdeea9
Matt_Tomlinson <mbrucetomlinson@gmail.com>
2017-03-02 21:34:27 -0500

    got the template working in Django

--- onepager/urls.py
+++ onepager/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from personal import views
+
 urlpatterns = [
+    url(r'^index/', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
